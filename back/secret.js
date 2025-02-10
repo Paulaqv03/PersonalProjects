d29e91a8f36294a3800d5801f8fce034e3c016fc
Paulaqv03 <paulitaqv03@gmail.com>
2025-02-10 15:41:28 -0500

    Starting new repository

--- back/secret.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-// Genera una clave secreta de 256 bits (32 bytes) codificada en hexadecimal
-const secretKey = crypto.randomBytes(32).toString('hex');
-console.log(secretKey);
