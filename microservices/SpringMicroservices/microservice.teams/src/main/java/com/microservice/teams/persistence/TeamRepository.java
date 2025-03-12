5a9a935564a180b392eec5b9cf23263f8ab4a839
Paulaqv03 <paulitaqv03@gmail.com>
2025-03-11 21:42:15 -0500

    DB configurations

--- microservices/SpringMicroservices/microservice.teams/src/main/java/com/microservice/teams/persistence/TeamRepository.java
+++ microservices/SpringMicroservices/microservice.teams/src/main/java/com/microservice/teams/persistence/TeamRepository.java
@@ -4,8 +4,6 @@ import com.microservice.teams.entities.Team;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
 }
